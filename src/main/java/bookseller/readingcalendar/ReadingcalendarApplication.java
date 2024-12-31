ad9110b547b52048e05e4658ff2927fa2b02fee1
hoooonshub <yhn515@naver.com>
2024-12-31 16:49:50 +0900

    init

---
+++ src/main/java/bookseller/readingcalendar/ReadingcalendarApplication.java
@@ -0,0 +1,13 @@
+package bookseller.readingcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingcalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingcalendarApplication.class, args);
+	}
+
+}
